611dceef4cbf8299937b29690142dece3a148057
erigdip <alejoacosta1612@gmail.com>
2023-09-12 17:45:59 -0400

    misc changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById('MenuList');
 const menu = document.getElementById('Menu');
+
 menu.addEventListener('click', function () {
   'use strict';
   nav.classList.toggle('show');
