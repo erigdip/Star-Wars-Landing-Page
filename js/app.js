890a45e1fc21ed689ad1c50f77d8ee77d742e445
erigdip <alejoacosta1612@gmail.com>
2023-09-13 13:06:34 -0400

    js - youtube player added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,43 @@
 const nav = document.getElementById('MenuList');
 const menu = document.getElementById('Menu');
 
+const questionMark = document.getElementById('question-mark');
+const summaryVid = document.getElementById('summary-video');
+
 menu.addEventListener('click', function () {
   'use strict';
   nav.classList.toggle('show');
 });
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '315',
+    width: '560',
+    videoId: 'VRZZNuEniyo',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
